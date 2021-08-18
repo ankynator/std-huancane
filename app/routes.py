695a4bde06a75e50a17f06987556e523c9dddd09
ankynator <ankynator@gmail.com>
2021-08-18 00:48:50 -0500

    Implementando el authenticate con flask-login

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 import datetime
 from app import app
+from app import db
 from flask import render_template, redirect, url_for, flash
 from app.models import File, User
 from app.forms import SearchFileForm, RegisterFileForm, LoginUserForm, RegisterUserform
-from app import db
+from flask_login import login_user, logout_user, login_required, current_user
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/home', methods=['POST', 'GET'])
@@ -68,31 +69,35 @@ def view_file_page(file_number):
     return render_template('file.html', attempend_file=attempend_file, time=only_time, date=only_date)
 
 @app.route('/files')
+@login_required
 def files_page():
     files = File.query.all()
     return render_template('files.html', files = files)
 
-@app.route('/login', methods=['POST', 'GET'])
-def login_user():
+@app.route('/login', methods=['GET', 'POST'])
+def login_user_page():
     form = LoginUserForm()
     if form.validate_on_submit():
-        attemp_user = User.query.filter_by(username=form.username.data).first()
-        if attemp_user and attemp_user.check_password_correction(attempted_password=form.password.data):
-            flash(f'Inicio de sesion satisfactorio {attemp_user.username}', category='success')
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+            attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Inicio de sesion satisfactorio {attempted_user.username}', category='success')
             return redirect(url_for('files_page'))
         else:
             flash('El nombre de usuario y la contraseña no coinciden! por favor intente de nuevo', category='danger')
     return render_template('login_user.html', form=form)
 
 @app.route('/register_user', methods=['POST', 'GET']) 
-def register_user():
+def register_user_page():
     form = RegisterUserform()
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
                               password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
-
+        login_user(user_to_create)
         flash(f'Usuario registrado correctamente! Bienvenido {user_to_create.username}', category='success')
         return redirect(url_for('files_page'))
     if form.errors != {}:
@@ -100,3 +105,10 @@ def register_user():
             flash(f'Hubo un error registrando al usuario: {err_msg}', category='danger')
 
     return render_template('register_user.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("Se cerro sesion correctamente!", category='info')
+
+    return redirect(url_for("home_page"))
